d35e4fdb20825c83a4fac40ce9cb074346f6aca9
Hoterpoter <sensei.lis@yandex.com>
2024-05-06 14:44:56 +0300

    initial commit

---
+++ task_11.py
@@ -0,0 +1,28 @@
+'''Дано натуральное число A > 1. Определите, каким по
+счету числом Фибоначчи оно является, то есть
+выведите такое число n, что φ(n)=A. Если А не
+является числом Фибоначчи, выведите число -1.
+
+Input: 5
+Output: 6
+'''
+
+# 0 1 1 2 3 5 8
+
+a = 5
+fibo_p, fibo_n = 0, 1
+position = 2
+while fibo_n < a:
+    fibo_p, fibo_n = fibo_n, fibo_p + fibo_n
+    position += 1
+if fibo_n == a:
+    print(position)
+else:
+    print(-1)
+
+def func(a, fibo_p=0, fibo_n=1, position=2):
+    if fibo_n == a:
+        return position
+    else:
+        
+    return func(a, fibo_n, fibo_p+fibo_n, position+1)
---
+++ task_25.py
@@ -0,0 +1,15 @@
+list_1 = [10, 21, 31, 40, 51]
+
+last = list_1.pop()
+first = list_1.pop(0)
+
+list_1.insert(0, last)
+list_1.append(first)
+
+print(list_1)
+
+my_dict = {1:1, 2:2}
+
+# a = 1
+# a = 123
+# a[2] = 5
