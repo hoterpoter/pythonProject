3cb487f89fbe363487b25b2c7fdc36a20cf85396
Hoterpoter <sensei.lis@yandex.com>
2024-06-08 01:50:51 +0300

    Домашнее задание OneHot вид

---
+++ task_44.py
@@ -0,0 +1,25 @@
+'''
+Задача 44: В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца.
+Ваша задача перевести его в one hot вид.
+Сможете ли вы это сделать без get_dummies?
+'''
+
+# Попытка
+import pandas as pd
+import random
+
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+
+data['is_robot'] = (data['whoAmI'] == 'robot').astype(int) #Преобразование в one hot вид
+data['is_human'] = (data['whoAmI'] == 'human').astype(int)#Преобразование в one hot вид
+
+data = data.drop('whoAmI', axis=1) #Удаляем ненужный столбец
+data.to_csv('whoami.csv', index=False)# Записываем все в файл
+
+data = pd.read_csv('whoami.csv')
+
+print(data.head())
